d55559c9ab4a9de7e346960ff804c3d416a0cc62
YassineGallaoui <yassine.gallaoui.dev@gmail.com>
2025-02-24 23:29:20 +0100

    update Footer styles and content; add last update note and improve layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,19 +5,20 @@ const Footer = () => {
     return (
         <footer className={style.footer + ' mt-4'}>
             <div className='container pt-4'>
-                <div className={"flex flex-col col-4 col-md-4 col-lg-6 mb-4"}>
+                <div className={style.container + " flex flex-col col-4 col-md-4 col-lg-6 mb-4"}>
                     <span className={'text-bold'}>{`
                     Audio Toggles`}</span>
-                    <span style={{ marginTop: "0.3rem" }}>{`Handcrafted by `}
+                    <span style={{ marginTop: "1rem" }}>{`Handcrafted by `}
                         <Link
                             href="https://www.yassinegallaoui.com/"
                             target="_blank" rel="noopener noreferrer">
                             {`Yas`}
                         </Link>
                     </span>
+                    <span>{`Last update: 24 Feb 2025`}</span>
                 </div>
 
-                <div className={"flex flex-col col-4 col-md-4 col-lg-6 mb-4"}>
+                <div className={style.container + " flex flex-col col-4 col-md-4 col-lg-6 mb-4"}>
                     <span>{`Animations are made at code level`}</span>
                     <span>{`Some are made with the help of `}
                         <Link
@@ -28,6 +29,7 @@ const Footer = () => {
                     </span>
                     <span>{`Some are made using (S)css in a relatively intense way`}</span>
                     <span>{`Running on Next.js v15.1.7`}</span>
+                    <span>{`... more to come`}</span>
                 </div>
             </div>
         </footer>
